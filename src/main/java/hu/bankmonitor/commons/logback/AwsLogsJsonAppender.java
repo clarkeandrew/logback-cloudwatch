493e5f7ad31c12924c7412afa42b855ea17d326e
Andrew Clarke <clarkeandrew@gmail.com>
2015-08-16 13:27:25 +0100

    allow for aws request limit of 5 p/s

--- src/main/java/hu/bankmonitor/commons/logback/AwsLogsJsonAppender.java
+++ src/main/java/hu/bankmonitor/commons/logback/AwsLogsJsonAppender.java
@@ -27,120 +27,101 @@ import lombok.Setter;
 @Setter
 public class AwsLogsJsonAppender extends AppenderBase<ILoggingEvent> {
 
-	static enum BatchMode {
-		none,
-		seconds,
-		count
-	};
+    static int MIN_INTERVAL_MILLIS = 200;
 
-	private final ObjectMapper om;
+    private final ObjectMapper om;
 
-	private AWSLogsClient awsLogsClient;
+    private AWSLogsClient awsLogsClient;
 
-	private String lastSequenceToken;
+    private String lastSequenceToken;
 
-	private String awsAccessKey;
+    private String awsAccessKey;
 
-	private String awsSecretKey;
+    private String awsSecretKey;
 
-	private String awsRegionName;
+    private String awsRegionName;
 
-	private String logGroupName = "test-log-group";
+    private String logGroupName = "test-log-group";
 
-	private String logStreamName = "test-log-stream";
+    private String logStreamName = "test-log-stream";
 
-	private BatchMode batchMode = BatchMode.none;
+    private long lastUpdateMillis = 0;
 
-	private int batchModeValue = 0;
+    List<InputLogEvent> logEvents;
 
-	private long lastUpdateMillis = 0;
+    public AwsLogsJsonAppender() {
+        logEvents = new LinkedList<>();
+        om = new ObjectMapper();
+        om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        JacksonUtils.registerModulesToObjectMapper(om);
+    }
 
-	List<InputLogEvent> logEvents;
-	public AwsLogsJsonAppender() {
-		logEvents = new LinkedList<>();
-		om = new ObjectMapper();
-		om.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-		JacksonUtils.registerModulesToObjectMapper(om);
-	}
+    @Override
+    protected void append(ILoggingEvent eventObject) {
 
-	@Override
-	protected void append(ILoggingEvent eventObject) {
+        try {
+            sendEvent(om.writeValueAsString(LogbackUtils.iLoggingEvent2Map(eventObject)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            addError("Error while sending a message", e);
+        }
+    }
 
-		try {
-			sendEvent(om.writeValueAsString(LogbackUtils.iLoggingEvent2Map(eventObject)));
-		} catch (Exception e) {
-			e.printStackTrace();
-			addError("Error while sending a message", e);
-		}
-	}
+    @Override
+    public void stop() {
+        if (logEvents.size() > 0) {
+            sendEventBatch();
+        }
+        super.stop();
+    }
 
-	@Override
-	public void stop() {
-		if (logEvents.size() > 0) {
-			sendEventBatch();
-		}
-		super.stop();
-	}
+    @Override
+    public void start() {
 
-	@Override
-	public void start() {
+        awsLogsClient = new AWSLogsClient(new StaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)));
+        if (awsRegionName != null) {
+            awsLogsClient.setRegion(Region.getRegion(Regions.fromName(awsRegionName)));
+        }
 
-		awsLogsClient = new AWSLogsClient(new StaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)));
-		if (awsRegionName != null) {
-			awsLogsClient.setRegion(Region.getRegion(Regions.fromName(awsRegionName)));
-		}
+        CreateLogGroupRequest createLogGroupRequest = new CreateLogGroupRequest(logGroupName);
+        try {
+            awsLogsClient.createLogGroup(createLogGroupRequest);
+        } catch (ResourceAlreadyExistsException e) {
+            addInfo("Log group " + logGroupName + " already exists");
+        }
 
-		CreateLogGroupRequest createLogGroupRequest = new CreateLogGroupRequest(logGroupName);
-		try {
-			awsLogsClient.createLogGroup(createLogGroupRequest);
-		} catch (ResourceAlreadyExistsException e) {
-			addInfo("Log group " + logGroupName + " already exists");
-		}
+        CreateLogStreamRequest createLogStreamRequest = new CreateLogStreamRequest(logGroupName, logStreamName);
+        try {
+            awsLogsClient.createLogStream(createLogStreamRequest);
+        } catch (ResourceAlreadyExistsException e) {
+            addInfo("Log stream " + logStreamName + "already exists");
+        }
 
-		CreateLogStreamRequest createLogStreamRequest = new CreateLogStreamRequest(logGroupName, logStreamName);
-		try {
-			awsLogsClient.createLogStream(createLogStreamRequest);
-		} catch (ResourceAlreadyExistsException e) {
-			addInfo("Log stream " + logStreamName + "already exists");
-		}
+        try {
+            sendEvent("Getting the next expected sequenceToken for AwsLogsAppender");
+        } catch (InvalidSequenceTokenException e) {
+            lastSequenceToken = e.getExpectedSequenceToken();
+        }
 
-		try {
-			sendEvent("Getting the next expected sequenceToken for AwsLogsAppender");
-		} catch (InvalidSequenceTokenException e) {
-			lastSequenceToken = e.getExpectedSequenceToken();
-		}
+        super.start();
+    }
 
-		super.start();
-	}
+    private void sendEvent(String message) {
+        addEventToBatch(message);
+        if (System.currentTimeMillis() - lastUpdateMillis > MIN_INTERVAL_MILLIS) {
+            sendEventBatch();
+            lastUpdateMillis = System.currentTimeMillis();
+        }
+    }
 
-	private void sendEvent(String message) {
-		addEventToBatch(message);
-		switch (batchMode) {
-			case count:
-				if (logEvents.size() >= batchModeValue) {
-					sendEventBatch();
-				}
-				break;
-			case seconds:
-				if (System.currentTimeMillis() - lastUpdateMillis > batchModeValue * 1000) {
-					sendEventBatch();
-					lastUpdateMillis = System.currentTimeMillis();
-				}
-				break;
-			default:
-				sendEventBatch();
-				break;
-		}
-	}
+    private void addEventToBatch(String message) {
+        logEvents.add(new InputLogEvent().withTimestamp(new Date().getTime()).withMessage(message));
+    }
 
-	private void addEventToBatch(String message) {
-		logEvents.add(new InputLogEvent().withTimestamp(new Date().getTime()).withMessage(message));
-	}
-
-	private void sendEventBatch() {
-		PutLogEventsRequest putLogEventsRequest = new PutLogEventsRequest(logGroupName, logStreamName, logEvents);
-		putLogEventsRequest.setSequenceToken(lastSequenceToken);
-		PutLogEventsResult putLogEventsResult = awsLogsClient.putLogEvents(putLogEventsRequest);
-		lastSequenceToken = putLogEventsResult.getNextSequenceToken();
-	}
+    private void sendEventBatch() {
+        PutLogEventsRequest putLogEventsRequest = new PutLogEventsRequest(logGroupName, logStreamName, logEvents);
+        putLogEventsRequest.setSequenceToken(lastSequenceToken);
+        PutLogEventsResult putLogEventsResult = awsLogsClient.putLogEvents(putLogEventsRequest);
+        lastSequenceToken = putLogEventsResult.getNextSequenceToken();
+    }
 }
